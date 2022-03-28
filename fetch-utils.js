115ca4dfe483ec69e37a63d6b40122fe4a9c4a9c
Will Gundy <willgunderson503@gmail.com>
2022-03-28 14:02:47 -0700

    added fetch utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -6,24 +6,38 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo) {
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
+    const response = await client
+        .from('todos')
+        .insert(todo)
+        .single();
 
     return checkError(response);
 }
 
 export async function deleteAllTodos() {
     // delete all todos for this user in supabase
+    const response = await client
+        .from('todos')
+        .delete(); 
 
     return checkError(response);
 }
 
 export async function getTodos() {
     // get all todos for this user from supabase
+    const response = await client
+        .from('todos')
+        .select('*'); 
 
     return checkError(response);
 }
 
 export async function completeTodo(id) {
     // find the and update (set complete to true), the todo that matches the correct id
+    const response = await client
+        .from('todos')
+        .update({ complete: true })
+        .match({ id: id }); 
 
     return checkError(response);
 }
@@ -38,13 +52,13 @@ export function checkAuth() {
     if (!user) location.replace('../');
 }
 
-export function redirectIfLoggedIn() {
+export async function redirectIfLoggedIn() {
     if (await getUser()) {
         location.replace('./todos');
     }
 }
 
-export function signupUser(email, password) {
+export async function signupUser(email, password) {
     const response = await client.auth.signUp({ email, password });
 
     return response.user;
